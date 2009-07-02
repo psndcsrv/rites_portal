d241b681720b1e67391f32d789a739f36a835488
Aaron Unger <aunger@concord.org>
2009-07-02 13:17:02 -0400

    Enable activescaffold routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace(:rites_portal) do |rites_portal|
-    rites_portal.resources :clazzes
-    rites_portal.resources :courses
-    rites_portal.resources :districts
-    rites_portal.resources :grade_levels
-    rites_portal.resources :learners
-    rites_portal.resources :offerings
-    rites_portal.resources :schools
-    rites_portal.resources :school_memberships
-    rites_portal.resources :semesters
-    rites_portal.resources :students
-    rites_portal.resources :student_clazzes
-    rites_portal.resources :subjects
-    rites_portal.resources :teachers
+    rites_portal.resources :clazzes, :active_scaffold => true 
+    rites_portal.resources :courses, :active_scaffold => true 
+    rites_portal.resources :districts, :active_scaffold => true 
+    rites_portal.resources :grade_levels, :active_scaffold => true 
+    rites_portal.resources :learners, :active_scaffold => true 
+    rites_portal.resources :offerings, :active_scaffold => true 
+    rites_portal.resources :schools, :active_scaffold => true 
+    rites_portal.resources :school_memberships, :active_scaffold => true 
+    rites_portal.resources :semesters, :active_scaffold => true 
+    rites_portal.resources :students, :active_scaffold => true 
+    rites_portal.resources :student_clazzes, :active_scaffold => true 
+    rites_portal.resources :subjects, :active_scaffold => true 
+    rites_portal.resources :teachers, :active_scaffold => true 
   end
   
 end
