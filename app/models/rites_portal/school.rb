777225422c606c96f8baa94979284d56ba22571f
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-07-04 15:24:13 -0400

    NEW MIGRATIONS for district and schools
    district and schools now belong_to their respective
    nces models

--- app/models/rites_portal/school.rb
+++ app/models/rites_portal/school.rb
@@ -2,6 +2,7 @@ class RitesPortal::School < ActiveRecord::Base
   set_table_name :rites_portal_schools
   
   belongs_to :district, :class_name => "RitesPortal::District", :foreign_key => "district_id"
+  belongs_to :nces_school, :class_name => "RitesPortal::Nces06School", :foreign_key => "nces_school_id"
   
   has_many :courses, :class_name => "RitesPortal::Course", :foreign_key => "school_id"
   has_many :semesters, :class_name => "RitesPortal::Semester", :foreign_key => "school_id"
