4d98911e0c0d59980823053b4fc5b1f784fc04f7
Scott Cytacki <scott@concord.org>
2009-07-06 13:10:26 -0400

    added an updated active_scaffold_controller_for which finds app's active scaffold controllers

--- app/controllers/rites_portal/application_controller.rb
+++ app/controllers/rites_portal/application_controller.rb
@@ -2,11 +2,14 @@ class RitesPortal::ApplicationController < ActionController::Base
   layout 'rites_portal_standard'
   
   def self.active_scaffold_controller_for(klass)
-    controller =  "#{klass.to_s().sub('::','::Scaffold::').pluralize()}Controller".constantize rescue controller = super
     
-    raise ActiveScaffold::ControllerNotFound, "#{controller} missing ActiveScaffold based on model: #{klass}", caller unless controller.uses_active_scaffold?
-    raise ActiveScaffold::ControllerNotFound, "ActiveScaffold on #{controller} is not for #{klass} model.", caller unless controller.active_scaffold_config.model == klass
+    # if a model class doesn't have a module then it is part of the containing
+    # app and we assume it has a module of Admin
+    if not /::/ === klass.to_s()
+       return internal_active_scaffold_controller_for(klass, klass.to_s(), "Admin::")
+    end
 
-    return controller
+    super  
   end
+  
 end
