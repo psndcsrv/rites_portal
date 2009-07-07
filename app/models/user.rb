bc5a491b1687d16d7d24c8831044990138ea00d4
Aaron Unger <aunger@concord.org>
2009-07-07 12:31:58 -0400

    Remove investigations-specific code from the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,37 +28,15 @@ class User < ActiveRecord::Base
 
   # Relationships
   has_and_belongs_to_many :roles
-  has_many :investigations
-  has_many :activities
-  has_many :sections
-  has_many :pages
-  
-  has_many :data_collectors
-  has_many :xhtmls
-  has_many :open_responses
-  has_many :multiple_choices
-  has_many :data_tables
-  has_many :drawing_tools
-  has_many :mw_modeler_pages
-  has_many :n_logo_models
   
   has_one :portal_teacher, :class_name => "RitesPortal::Teacher"
   has_one :portal_student, :class_name => "RitesPortal::Student"
   
+  has_one :sds_config, :class_name => "RitesPortal::SdsConfig", :as => :configurable
+  
   belongs_to :vendor_interface
 
-  # has_many :assessment_targets
-  # has_many :big_ideas
-  # has_many :domains
-  # has_many :expectations
-  # has_many :expectation_stems
-  # has_many :grade_span_expectations
-  # has_many :knowledge_statements
-  # has_many :unifying_themes
-
   acts_as_replicatable
-
-  include Changeable
   
   self.extend SearchableModel
   
@@ -69,7 +47,6 @@ class User < ActiveRecord::Base
       @@searchable_attributes
     end
   end
-  
 
   # we will lazy load the anonymous user later
   @@anonymous_user = nil 
@@ -91,16 +68,6 @@ class User < ActiveRecord::Base
   def name
     _fullname = "#{first_name} #{last_name}"
     _fullname.strip != "" ? _fullname : login
-  end
-  
-  def removed_investigation
-    unless self.has_investigations?
-      self.remove_role('author')
-    end
-  end
-  
-  def has_investigations?
-    investigations.length > 0
   end
 
   # Check if a user has a role.
@@ -138,11 +105,6 @@ class User < ActiveRecord::Base
 
   def make_user_a_member
     roles << Role.find_by_title('member')
-  end
-
-  # return the user who is the site administrator
-  def self.site_admin
-    User.find_by_email(APP_CONFIG[:admin_email])
   end
   
   # is this user the anonymous user?
