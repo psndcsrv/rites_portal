c473c56d5671881de55ed07e89069cd9074b51d3
Aaron Unger <aunger@concord.org>
2009-07-07 12:06:39 -0400

    Instantiate the user observer in the plugin.

--- init.rb
+++ init.rb
@@ -9,6 +9,7 @@ require File.join(File.dirname(__FILE__), '/lib/sds_connect.rb')
 # end
 
 require 'activesupport'
+require 'activerecord'
 
 # setup the plugin so it gets reloaded in development mode
 ActiveSupport::Dependencies.load_once_paths.delete_if {|path| path.match("vendor/plugins/rites_portal")}
@@ -16,3 +17,8 @@ ActiveSupport::Dependencies.load_once_paths.delete_if {|path| path.match("vendor
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'clazz', 'clazzes'
 end
+
+# :user_observer triggers activation emails to be sent when a user registers, and confirmation email to be sent when they activate
+
+ActiveRecord::Base.observers << :user_observer
+ActiveRecord::Base.instantiate_observers
