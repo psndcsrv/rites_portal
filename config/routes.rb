67de546e6f89b28978f7c9ff383bb234c9934839
Aaron Unger <aunger@concord.org>
2009-07-07 11:42:08 -0400

    Move the users and sessions into the portal. Cleaned up the default layouts so that the rites investigations layout is used.

--- config/routes.rb
+++ config/routes.rb
@@ -33,4 +33,26 @@ ActionController::Routing::Routes.draw do |map|
     
   end
   
+  # Restful Authentication Rewrites
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
+  map.forgot_password '/forgot_password', :controller => 'passwords', :action => 'new'
+  map.change_password '/change_password/:reset_code', :controller => 'passwords', :action => 'reset'
+  map.open_id_complete '/opensession', :controller => "sessions", :action => "create", :requirements => { :method => :get }
+  map.open_id_create '/opencreate', :controller => "users", :action => "create", :requirements => { :method => :get }
+  
+  # Restful Authentication Resources
+  map.resources :users, :member => { 
+    :preferences => [:get, :put], 
+    :interface => :get,
+    :suspend   => :put,
+    :unsuspend => :put,
+    :purge     => :delete }
+    
+  map.resources :passwords
+  map.resource :session
+  
 end
