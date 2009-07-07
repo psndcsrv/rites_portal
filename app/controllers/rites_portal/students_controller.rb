2ba574d525c1124c51ac762ff049d453a1243118
Aaron Unger <aunger@concord.org>
2009-07-07 17:40:34 -0400

    Student creation and editing form.

--- app/controllers/rites_portal/students_controller.rb
+++ app/controllers/rites_portal/students_controller.rb
@@ -25,7 +25,10 @@ class RitesPortal::StudentsController < ApplicationController
   # GET /rites_portal_students/new.xml
   def new
     @student = RitesPortal::Student.new
-
+    @user = User.new
+    if params[:clazz_id]
+      @clazz = RitesPortal::Clazz.find(params[:clazz_id])
+    end
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @student }
@@ -35,15 +38,38 @@ class RitesPortal::StudentsController < ApplicationController
   # GET /rites_portal_students/1/edit
   def edit
     @student = RitesPortal::Student.find(params[:id])
+    @user = @student.user
   end
 
   # POST /rites_portal_students
   # POST /rites_portal_students.xml
   def create
-    @student = RitesPortal::Student.new(params[:student])
+    success = false
+    begin
+      user_params = params[:user]
+      user_params[:login] = RitesPortal::Student.generate_user_login(user_params[:first_name], user_params[:last_name])
+      user_params[:email] = RitesPortal::Student.generate_user_email()
+      @user = RitesPortal::User.create!(user_params)
+      @user.register!
+      @user.activate!
 
+      student_params = params[:student]
+      student_params[:name] = @user.first_name + " " + @user.last_name
+      @student = RitesPortal::Student.new(student_params)
+      if params[:clazz_id]
+        @clazz = RitesPortal::Clazz.find(params[:clazz][:id])
+      end
+      @student.save!
+      
+      if @clazz
+        @student.student_clazzes.create!(:clazz_id => @clazz.id, :start_time => Time.now)
+      end
+      
+      success = true
+    rescue
+    end
     respond_to do |format|
-      if @student.save
+      if success
         flash[:notice] = 'RitesPortal::Student was successfully created.'
         format.html { redirect_to(@student) }
         format.xml  { render :xml => @student, :status => :created, :location => @student }
--- app/models/rites_portal/teacher.rb
+++ app/models/rites_portal/teacher.rb
@@ -5,6 +5,10 @@ class RitesPortal::Teacher < ActiveRecord::Base
   
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   
+  # because of has_many_polymorphs, we don't need the following relationships defined
+  # has_many :school_memberships
+  # has_many :schools, :through => :school_memberships
+  
   has_many :subjects, :class_name => "RitesPortal::Subject", :foreign_key => "teacher_id"
   has_many :clazzes, :class_name => "RitesPortal::Clazz", :foreign_key => "teacher_id"
   
