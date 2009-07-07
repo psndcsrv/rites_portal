8bde92d260e101b26a222ab1b0803cf1f5cd75b0
Aaron Unger <aunger@concord.org>
2009-07-07 13:39:19 -0400

    Have to actually run setup on sds_connect

--- init.rb
+++ init.rb
@@ -2,6 +2,8 @@
 require File.join(File.dirname(__FILE__), '/lib/rites_portal.rb')
 require File.join(File.dirname(__FILE__), '/lib/sds_connect.rb')
 
+RitesPortal::SdsConnect::Connect.setup
+
 # Using a separate DB isn't going to work because of the various join tables we'll have to make to the existing app's db
 # begin
 #   RitesPortal.configure_connections(:rites_portal)
