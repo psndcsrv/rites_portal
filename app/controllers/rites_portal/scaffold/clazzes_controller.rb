16ca71a734744cad65986211773bc26c82345947
Noah Paessel <knowuh@gmail.com>
2009-07-07 11:20:18 -0400

    Just like students, clazzes can't handle the through relationship in active-scaffold.

--- app/controllers/rites_portal/scaffold/clazzes_controller.rb
+++ app/controllers/rites_portal/scaffold/clazzes_controller.rb
@@ -1,3 +1,6 @@
 class RitesPortal::Scaffold::ClazzesController < RitesPortal::ApplicationController
-  active_scaffold "RitesPortal::Clazz"
+  active_scaffold "RitesPortal::Clazz" do |config|
+    # this was causing problems because active scaffold is looking for a student_id on the students.  
+    config.columns.exclude :students
+  end
 end
