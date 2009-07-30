a18008929edc18736a063db4287200cc6346cc76
Noah Paessel <knowuh@gmail.com>
2009-07-30 19:23:36 -0400

    methods to allow setting schools by id values.

--- app/models/portal/teacher.rb
+++ app/models/portal/teacher.rb
@@ -15,10 +15,21 @@ class Portal::Teacher < ActiveRecord::Base
   
   has_and_belongs_to_many :grade_levels, :join_table => "portal_grade_levels_teachers", :class_name => "Portal::GradeLevel"
   
-  [:name, :first_name, :last_name, :email, :vendor_interface].each { |m| delegate m, :to => :user }
+  [:name, :first_name, :login, :password, :last_name, :email, :vendor_interface].each { |m| delegate m, :to => :user }
   
   include Changeable
   
+  
+  ##
+  ##
+  ##
+  def school_ids
+    schools.map { |s| s.id }
+  end
+  
+  def school_ids=(ids)
+    self.schools = ids.map { |i| Portal::School.find(i)}
+  end
   
   ##
   ##
